43af4eec5eacc7b8ba582263443fd5c86e4a6840
CrisMarin1211 <alca81411@gmail.com>
2024-03-13 11:55:51 -0500

    n

---
+++ src/components/indexPadre.ts
@@ -0,0 +1,1 @@
+export { default as myComponents } from './myComponents/myComponents';
---
+++ src/indexAbuelo.ts
@@ -0,0 +1,39 @@
+import * as MyComponents from './components/indexPadre';
+import Mycomponent, { Attribute } from './components/myComponents/myComponents';
+import { workers } from './data/data';
+class AppContainer extends HTMLElement {
+	myComponents: Mycomponent[] = [];
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: `open` });
+
+		workers.forEach((user) => {
+			const myComponentsCard = this.ownerDocument.createElement(`My-Component`) as Mycomponent;
+			myComponentsCard.setAttribute(Attribute.name, user.name);
+			myComponentsCard.setAttribute(Attribute.uid, String(user.id));
+			myComponentsCard.setAttribute(Attribute.image, user.image);
+			myComponentsCard.setAttribute(Attribute.age, String(user.age));
+			myComponentsCard.setAttribute(Attribute.gender, user.gender);
+			myComponentsCard.setAttribute(Attribute.area, user.jobDetails.area);
+			myComponentsCard.setAttribute(Attribute.position, user.jobDetails.position);
+			myComponentsCard.setAttribute(Attribute.timeInCompany, String(user.jobDetails.timeInCompany));
+			myComponentsCard.setAttribute(Attribute.experience, String(user.jobDetails.experience));
+
+			this.myComponents.push(myComponentsCard);
+		});
+	}
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		if (this.shadowRoot) {
+			this.myComponents.forEach((MyComponent) => {
+				this.shadowRoot?.appendChild(MyComponent);
+			});
+		}
+	}
+}
+
+customElements.define('app-container', AppContainer);
